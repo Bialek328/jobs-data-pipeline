05325ff5a5abbc5b388b6286d4de94078cd49059
Mateusz <bialek328@gmail.com>
2024-09-22 15:19:40 +0200

    scraping tech stack

--- go_scraper/main.go
+++ go_scraper/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-    "log"
+	"log"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
+type JobOffer struct {
+    Url string
+    Date string
+    TechStack []string
+    Experience string
+}
+
 func main() {
+    var techStack []string
     log.Println("Starting collector")
     c := colly.NewCollector()
 
-    c.OnHTML("div.MuiBox-root", func(e *colly.HTMLElement) {
-        // inner := e.ChildText(".MuiTypography-root")
-        inner := e.Text
-        log.Println("content:", inner)
+    jobCollector := colly.NewCollector()
+
+
+    c.OnHTML("div.MuiBox-root a", func(e *colly.HTMLElement) {
+        inner := e.Attr("href")
+        if inner != "" && strings.Contains(inner, "/offers/"){
+            jobCollector.Visit(e.Request.AbsoluteURL(inner))
+        }
     })
 
-    c.Visit("https://justjoin.it/trojmiasto")
+    jobCollector.OnHTML("div.MuiBox-root.css-jfr3nf", func(e *colly.HTMLElement) {
+        e.ForEach("h4.MuiTypography-root.MuiTypography-subtitle2.css-7de92v", func(_ int, el *colly.HTMLElement) {
+            // log.Println(el.Text)
+            techStack = append(techStack, el.Text)
+        })
+    })
+    
+    jobCollector.OnRequest(func(r *colly.Request) {
+        log.Println("visiting:", r.URL)
+    })      
+
+    jobCollector.OnScraped(func(r *colly.Response) {
+        log.Println(techStack)
+        techStack = []string{}
+    })
+    
+    c.Visit("https://justjoin.it/trojmiasto/python")
 }
